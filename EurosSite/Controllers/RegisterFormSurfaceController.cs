bbc89f936695cfef586807778bbf09fa9f48f150
EWEB\ben.martin <benmartin494@gmail.com>
2018-09-07 09:42:03 +0100

    inital commit

---
+++ EurosSite/Controllers/RegisterFormSurfaceController.cs
@@ -0,0 +1,31 @@
+﻿//using EurosSite.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Mail;
+//using System.Web;
+//using System.Web.Mvc;
+//using Umbraco.Web.Mvc;
+
+//namespace EurosSite.Controllers
+//{
+//    public class RegisterFormSurfaceController : SurfaceController
+//    {
+//        // GET: RegisterFormSurface
+//        public ActionResult Index()
+//        {
+//            return PartialView("RegisterForm", new RegisterFormViewModel());
+//        }
+
+//        [HttpPost]
+//        public ActionResult HandleFormSumbit(RegisterFormViewModel model)
+//        {
+//            //send email
+//            MailMessage message = new MailMessage();
+//            message.To.Add("ben.martin@madetoengage.com");
+//            message.Subject = "New event entry registered";
+//            message.From = new System.Net.Mail.MailAddress(model.EmailAddress);
+//            message.Body = model.FirstName, m
+//        }
+//    }
+//}
---
+++ EurosSite/Models/RegisterFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EurosSite.Models
+{
+    public class RegisterFormViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string HomeAddress { get; set; }
+        public string Country { get; set; }
+        public string Club { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
